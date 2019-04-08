5880985ee6cf8421b1fc63810c94f513e612b23b
Cassio Castro <cassiocastro.ti@gmail.com>
2019-04-08 17:02:56 -0300

    Completed the module

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.post('/transfer', (req, res) => {
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
-    accounts.credit.avaliable += parseInt(req.body.amount, 10);
+    accounts.credit.available += parseInt(req.body.amount, 10);
 
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
